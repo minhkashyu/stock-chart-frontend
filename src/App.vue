2db91ecf6447ce91fe691f270b4608aa95f40600
minhkashyu <minhvinhta@yahoo.com>
2017-09-23 20:50:23 +1000

    bottom gap

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,18 @@
   @import url("https://fonts.googleapis.com/css?family=Roboto");
   @import url("./../semantic/dist/semantic.min.css");
 
+  html, body {
+    height: 100%;
+  }
+
   body {
     background: #213e46 url('./assets/stock-market.jpg') no-repeat center center fixed !important;
     -webkit-background-size: cover !important;
     -moz-background-size: cover !important;
     -o-background-size: cover !important;
     background-size: cover !important;
-    padding-bottom: 20px;
   }
+  
   #app {
     font-family: "Roboto", "Helvetica", sans-serif;
     -webkit-font-smoothing: antialiased;
