f9e0ca773620c28bc8ffb00dbeb6d63db7e6e4aa
minhkashyu <minhvinhta@yahoo.com>
2017-09-23 20:35:28 +1000

    bottom gap

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,10 @@
   @import url("./../semantic/dist/semantic.min.css");
 
   body {
-    background: #213e46 url('./assets/stock-market.jpg') no-repeat fixed center !important;
+    background: #213e46 url('./assets/stock-market.jpg') no-repeat center center fixed !important;
+    -webkit-background-size: cover !important;
+    -moz-background-size: cover !important;
+    -o-background-size: cover !important;
     background-size: cover !important;
   }
   #app {
